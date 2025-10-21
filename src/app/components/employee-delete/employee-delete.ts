03e88d46eb29a378de5704fcfb39238be22c5118
Enrique Ejeda <enriqueejeda2016@gmail.com>
2025-10-21 23:34:01 +0200

    estructura basica de la app

---
+++ src/app/components/employee-delete/employee-delete.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-delete',
+  imports: [],
+  templateUrl: './employee-delete.html',
+  styleUrl: './employee-delete.css'
+})
+export class EmployeeDelete {
+
+}
